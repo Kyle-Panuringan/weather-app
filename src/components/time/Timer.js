0ed14822732c024db8cee8df52f39cc23b2ec4b1
Kyle Panuringan <kylepanuringan@gmail.com>
2022-07-09 21:28:22 +0800

    Add year, month, date, and weekday in Timer component

--- src/components/time/Timer.js
+++ src/components/time/Timer.js
@@ -3,12 +3,27 @@ import "./timer.css";
 
 const Timer = () => {
 	const [clock, setClock] = React.useState();
+	const [today, setToday] = React.useState();
 
 	React.useEffect(() => {
+		const weekdays = [
+			"Sunday",
+			"Monday",
+			"Tuesday",
+			"Wednesday",
+			"Thursday",
+			"Friday",
+			"Saturday",
+		];
 		const interval = setInterval(() => {
 			const date = new Date();
-			console.log("Date", date);
-			setClock(date.toLocaleTimeString());
+			const year = date.getFullYear();
+			const month = date.getMonth();
+			const currentdate = date.getDate();
+			const weekday = weekdays[date.getDay()];
+			const time = date.toLocaleTimeString();
+			setClock(time);
+			setToday(`${weekday}, ${month}/${currentdate}/${year}`);
 		}, 1000);
 		return () => {
 			clearInterval(interval);
@@ -18,6 +33,7 @@ const Timer = () => {
 	return (
 		<div className="timer-container">
 			<h1>{clock}</h1>
+			<h2>{today}</h2>
 		</div>
 	);
 };
